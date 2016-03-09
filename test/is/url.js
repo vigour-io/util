dcaa8596fcd80b6977478075c90dbce53bfc6650
Shawn Inder <shawn@vigour.io>
2016-03-09 17:36:11 +0100

    fix test messages

--- test/is/url.js
+++ test/is/url.js
@@ -5,16 +5,17 @@ var isURL = require('../../is/url')
 
 var testCases = [
 // ['url',expectedResult]
-  ['boom', false, 'simple string'],
-  ['perdu.com', true, 'domain.ext'],
-  ['http://domain.ext/path?query=string', true, 'with query string'],
-  ['https://boom.com/path?query=string%20with%20spaces', true, 'with (uri encoded) spaces (%20) in query string'],
-  ['https://boom.com/path?query=string%20with%20spaces#andThis', true, 'with hash']
+  ['string', false],
+  ['domain.ext', true],
+  ['http://domain.ext/path?query=string', true],
+  ['https://domain.ext/path?query=string%20with%20spaces', true],
+  ['https://domain.ext/path#withHash', true],
+  ['http://domain.ext/path?query=string#withHash', true]
 ]
 
 test('isURL', function (t) {
   t.plan(testCases.length)
   testCases.map(function (item) {
-    t.equals(isURL(item[0]), item[1])
+    t.equals(isURL(item[0]), item[1], item[0])
   })
 })
