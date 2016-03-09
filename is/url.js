d7a659ce14c2bcd48c88ac4eb42aff1aae9e0edf
Jim de Beer <jim@vigour.io>
2016-03-09 19:30:40 +0100

    url is fixed

--- is/url.js
+++ is/url.js
@@ -1,5 +1,5 @@
 'use strict'
-var url = /^(((ws(s)?)|(http(s)?))\:\/\/)?[a-zA-Z0-9_-]+(\.|\:)([^\/\/])[a-zA-Z/0-9$-/:-?{-~!"^_`\[\]]+$/
+var url = /^(((ws(s)?)|(http(s)?))\:\/\/)?[a-zA-Z0-9_-]+(\.|\:)([^\/\/])[a-zA-Z/0-9$-/:-?{#-~!"^_`\[\]]+$/
 module.exports = function (val) {
-  return typeof val === 'string' && url.test(val)
+  return typeof val === 'string' && url.test(val) || val === 'localhost'
 }
--- test/is/url.js
+++ test/is/url.js
@@ -1,5 +1,4 @@
 'use strict'
-
 var test = require('tape')
 var isURL = require('../../is/url')
 
@@ -10,12 +9,14 @@ var testCases = [
   ['http://domain.ext/path?query=string', true],
   ['https://domain.ext/path?query=string%20with%20spaces', true],
   ['https://domain.ext/path#withHash', true],
-  ['http://domain.ext/path?query=string#withHash', true]
+  ['http://domain.ext/path?query=string#withHash', true],
+  ['localhost', true],
+  ['localhost:8080', true]
 ]
 
 test('isURL', function (t) {
   t.plan(testCases.length)
-  testCases.map(function (item) {
+  testCases.forEach(function (item) {
     t.equals(isURL(item[0]), item[1], item[0])
   })
 })
