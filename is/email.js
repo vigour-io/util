8e838179547cf062b101d873e819db0d561caedc
Shawn Inder <shawn@vigour.io>
2016-03-10 21:22:12 +0100

    Added special chars and corrected max lengths

--- is/email.js
+++ is/email.js
@@ -1,5 +1,5 @@
 'use strict'
-var email = /^[a-zA-Z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-zA-Z]{2,20}$/
+var email = /^[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~.]{1,64}@[A-Za-z0-9.-]{1,64}\.[a-zA-Z]{2,20}$/
 module.exports = function (val) {
   return typeof val === 'string' && email.test(val)
 }
