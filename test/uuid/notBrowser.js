ea33db2530de42f5f7a5f37a369ba52af7dec47d
Shawn Inder <shawn@vigour.io>
2016-03-11 22:15:43 +0100

    Added test for uuid

--- test/uuid/notBrowser.js
+++ test/uuid/notBrowser.js
@@ -1,9 +1,10 @@
 'use strict'
 var test = require('tape')
-var uuid = require('../../uuid')
+var uuid = require('../../uuid/')
 var isHash = require('../../is/hash')
 
-test('uuid (browser)', function (t) {
-  t.plan(1)
-  t.equals(isHash(uuid()), true, 'isHash(uuid()) === ' + true)
+test('uuid (not browser)', function (t) {
+  t.plan(2)
+  t.equals(isHash(uuid.val), true, 'isHash(uuid.val) === true')
+  t.equals(isHash(uuid.generate()), true, 'isHash(uuid.generate()) === true')
 })
