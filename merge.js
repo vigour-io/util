3c4806d3cebd69570b7706be1de488917e46eb2d
Shawn Inder <shawn@vigour.io>
2016-03-13 12:58:55 +0100

    use `console.log` instead of `console.warn` for deprecation notice

--- merge.js
+++ merge.js
@@ -2,7 +2,7 @@
 const OBJ = 'object'
 
 module.exports = function merge (a, b, checked) {
-  console.warn('`vigour-util/merge` is deprecated, consider using `lodash.merge` instead')
+  console.log('WARNING: `vigour-util/merge` is deprecated, consider using `lodash.merge` instead')
   if (checked || typeof a === OBJ) {
     for (let key in b) {
       let bProp = b[key]
