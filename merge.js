42bf79d0ae7ab01dc19c6b3510d8736d796e4a70
Shawn Inder <shawn@vigour.io>
2016-03-13 12:57:59 +0100

    deprecate merge

--- merge.js
+++ merge.js
@@ -2,6 +2,7 @@
 const OBJ = 'object'
 
 module.exports = function merge (a, b, checked) {
+  console.warn('`vigour-util/merge` is deprecated, consider using `lodash.merge` instead')
   if (checked || typeof a === OBJ) {
     for (let key in b) {
       let bProp = b[key]
