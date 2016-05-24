7092c688461dc6d13e8f7dce43bbbd8075c7ad1b
Jim de Beer <jim@vigour.io>
2016-05-24 21:00:34 +0200

    coverage for isEmpty

--- test/is/empty.js
+++ test/is/empty.js
@@ -1,10 +1,11 @@
 'use strict'
-var test = require('tape')
-var Base = require('vigour-base')
-var isEmpty = require('../../is/empty')
-
-var testCases = [
+const test = require('tape')
+const Base = require('vigour-base')
+const isEmpty = require('../../is/empty')
+const testCases = [
 // ['object', expectedResult]
+  [{}, true],
+  [{ a: true }, false],
   [new Base({}), true],
   [new Base({ a: 'a' }), false],
   [new Base({ a: { val: null } }), true],
@@ -12,7 +13,7 @@ var testCases = [
   [new Base({ properties: { shawn: true }, shawn: {} }), true]
 ]
 
-test('isEmpty', function (t) {
+test('isEmpty - base', function (t) {
   t.plan(testCases.length)
   testCases.forEach(function (item) {
     t.equals(isEmpty(item[0]), item[1], 'isEmpty(' + item[0] + ') === ' + item[1])
